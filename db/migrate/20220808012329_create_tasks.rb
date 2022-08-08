79e26b5db18d83e1fb18761a658c40534abebaaa
kanahebi <k.miyasaka@mof-mof.co.jp>
2022-08-08 10:46:12 +0900

    Taskモデルを追加

---
+++ db/migrate/20220808012329_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tasks, id: :uuid do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_08_011950) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_08_012329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "tasks", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
