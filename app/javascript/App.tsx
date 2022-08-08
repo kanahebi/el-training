528671ffeb1a3a3c56d2fa91355cf3f408e60723
kanahebi <k.miyasaka@mof-mof.co.jp>
2022-08-08 18:45:53 +0900

    ReactからGraphQL呼び出し（csrf-token未対応）

--- app/javascript/App.tsx
+++ app/javascript/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AppRoutes } from './AppRoutes'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  credentials: 'include',
+  headers: {
+    'X-CSRFToken': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+  },
+  cache: new InMemoryCache()
+});
 
 export const App: React.FC = () => {
   return (
-    <Router>
-      <AppRoutes />
-    </Router>
+    <ApolloProvider client={client} >
+      <Router>
+        <AppRoutes />
+      </Router>
+    </ApolloProvider>
   )
 }
--- app/javascript/pages/Tasks.tsx
+++ app/javascript/pages/Tasks.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useQuery, gql } from "@apollo/client";
+
+const GET_TASKS = gql`
+  query GetTasks {
+    tasks {
+      id
+      name
+      description
+    }
+  }
+`;
 
 export const Tasks = () => {
+  const {loading, error, data} = useQuery(GET_TASKS);
+
+  if (loading) return (<div>'ロード中....'</div>);
+  if (error) return (<div>`Error ${error.message}`</div>);
+
   return (
     <div>
-      <h1>これはタスク一覧ページです。</h1>
+      {data.tasks.map(task => (
+        <div key={task.id}>
+          <h1>{task.name}</h1>
+          <div>{task.description}</div>
+        </div>
+      ))}
     </div>
   )
-}
+};
