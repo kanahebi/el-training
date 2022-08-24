fad6afa36c559e06ff94430d07f7a0fe57b7b358
kanahebi <k.miyasaka@mof-mof.co.jp>
2022-08-24 12:16:18 +0900

    Capybaraを使えるように設定

---
+++ spec/support/capybara.rb
@@ -0,0 +1,30 @@
+require 'capybara/rspec'
+
+Capybara.register_driver :remote_chrome do |app|
+  url = ENV.fetch("SELENIUM_DRIVER_URL")
+  caps = ::Selenium::WebDriver::Remote::Capabilities.chrome(
+    "goog:chromeOptions" => {
+      "args" => [
+        "no-sandbox",
+        "headless",
+        "disable-gpu",
+        "disable-dev-shm-usage",
+        "window-size=1680,1050"
+      ]
+    }
+  )
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: url, capabilities: caps)
+end
+Capybara.javascript_driver = :remote_chrome
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    Capybara.server_host = 'web-server'
+    Capybara.server_port = "4444"
+    driven_by :remote_chrome
+  end
+end
