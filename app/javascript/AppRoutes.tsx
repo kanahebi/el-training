a64d099ebe696f480d96ddb6d943d43f0c71cadd
kanahebi <k.miyasaka@mof-mof.co.jp>
2022-09-08 12:02:06 +0900

    使っていない定義を削除

--- app/javascript/AppRoutes.tsx
+++ app/javascript/AppRoutes.tsx
@@ -1,13 +1,9 @@
-import React, { FC } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { Tasks } from './pages/Tasks';
 import { TaskView } from './pages/TaskView';
 import { NewTask } from './pages/NewTask';
 import { EditTask } from './pages/EditTask';
-
-interface State {
-  background: string;
-};
 
 export const AppRoutes = () => {
   return (
